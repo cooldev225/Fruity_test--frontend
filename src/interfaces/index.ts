c8a82f6e81e58873892e165f48ea1486be085974
cooldev225 <goldstar22501@gmail.com>
2023-03-17 17:58:35 +0900

    init vue element plus

---
+++ src/interfaces/index.ts
@@ -0,0 +1,12 @@
+export interface Fruit {
+    carbohydrade: number;
+    genus: string;
+    protein: number;
+    name: string;
+    fat: number;
+    calories: number;
+    id: number;
+    family: string;
+    sugar: number;
+    order: number;
+}
